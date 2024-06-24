0950c4d8221e536ea04ea4f6aa14ab83c8feef72
Bill Martin <will.uren@hotmail.com>
2024-06-24 09:49:48 +0800

    initial commit

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "PeopleClientRestApi",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secretPassword".Sha256())
+                    },
+                    AllowedScopes = {"peopleRestAPI"}
+                }
+            };
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("peopleRestAPI", "REST API for People")
+            };
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+
+            };
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+
+            };
+        public static List<TestUser> TestUsers =>
+            new List<TestUser>
+            {
+
+            };
+
+    }
+}
---
+++ IdentityServer/Program.cs
@@ -0,0 +1,21 @@
+using IdentityServer;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(Config.Clients)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+app.UseRouting();
+app.UseIdentityServer();
+    
+    
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
